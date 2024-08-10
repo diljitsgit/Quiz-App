666ad0a6f369a0c7c2f72c2879e150b25b9363c8
Diljit Singh <diljits1776@gmail.com>
2024-08-10 23:51:49 +0530

    Basic Functionality for Text Input Quiz App

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
-// const questionSpace = document.getElementById('question').innerHTML
-// const sumbit = document.getElementById('click')
-// const answer = document.getElementById('answer')
+function test(){
+    console.log('testing')
+}
+
+let questionSpace = document.getElementById('question')
+let submits = document.getElementById('click')
+let answer = document.getElementById('answer')
 
 async function fetchJSONData() {
     let res = await fetch("./data.json")
@@ -8,4 +12,57 @@ async function fetchJSONData() {
 }
 
 let temp = await fetchJSONData()
-console.log(temp)
+
+let timer
+let qNo = 0;
+let time=20;
+
+function start() {
+    timer = setInterval(
+        quiz,
+        1000
+    )
+}
+start()
+
+function quiz(){
+    ticktock()
+    getquestion()
+    submits.addEventListener("click", check);
+    if(qNo==temp.questions.length-1 && time==0){
+        quizEnd()
+    }
+    else if(time==0){
+        qNo++
+        timerReset()
+    }
+}
+
+function ticktock(){
+    time--
+    document.getElementById('timer').innerHTML=time
+}
+
+function check(){
+    if(answer.value==temp.questions[qNo].answer){
+        if(qNo==temp.questions.length-1){
+            quizEnd()
+        }
+        else{
+            qNo++
+            timerReset()
+        }
+    }
+}
+
+function quizEnd(){
+    clearInterval(timer)
+}
+
+function timerReset(){
+    time=20
+}
+
+function getquestion(){
+    questionSpace.innerHTML=temp.questions[qNo].question
+}
